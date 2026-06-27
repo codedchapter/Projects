01d05cb0cc230a689f291ece80a82555534d654b
Saff9 <saffanakbar942@gmail.com>
2026-06-27 22:06:41 +0530

    project3 , for now it is calcullator

---
+++ Project3/calculator.py
@@ -0,0 +1,18 @@
+import math
+radius = input(" What is radius? ")
+rea_circle = math.pi * int(radius) * int(radius)
+print(area_circle)
+
+radius = input(" What is radius? ")
+area_circle = math.pi * int(radius) ** 2
+print(area_circle)
+
+length = input(" Enter the lendth? ")
+bredth = input(" Ente bredth ?")
+
+area = int(length) * int(bredth)
+print(f" Area is {area}")
+
+minutes = input("How many Minutes? ")
+seconds = int(minutes) * 60
+print(f"{minutes} minutes  are equla to {seconds} seconds ")
